fbbf709399ae4b30cea5b74c9d9fec1c3d6372b5
bakhitaismail <bakhitaismailk@gmail.com>
2022-03-14 21:14:58 +0300

    first commit

---
+++ src/tryout.kt
@@ -0,0 +1,21 @@
+fun main(){
+    code("Kotlin")
+    modules(243, 575)
+    add(45, 56,67)
+    interesting()
+}
+fun code(name:String){
+    println("Hello Kotlin")
+}
+fun modules(a:Int, b:Int){
+    var modules=a%b
+    println(modules)
+
+}
+fun add(c:Int, d:Int, e:Int){
+    var add=(c+d+e)
+    println(add)
+}
+fun interesting(){
+    println("coding is fun")
+}
